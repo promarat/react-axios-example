3547fee9dacb011a13a05ca765e36ad09486f3fa
prodev-web <andreyyukhim@gmail.com>
2022-03-26 02:59:25 +0200

    change ngrok server

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "http://localhost:8080/api",
-    baseURL: "https://7d6b-188-43-136-44.ngrok.io/api",
+    baseURL: "https://1492-188-43-136-44.ngrok.io/api",
     headers: {
         "Content-type": "application/json"
     }
